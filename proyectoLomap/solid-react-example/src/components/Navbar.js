447c622be624761f8db3b05e9f3e1d712aa5951d
Aaron <Aaron@DESKTOP-8HG00E2>
2023-03-09 14:08:07 +0100

    Cambios en el nav, con iamgen y botón logout

--- proyectoLomap/solid-react-example/src/components/Navbar.js
+++ proyectoLomap/solid-react-example/src/components/Navbar.js
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
-import { LoginButton, LogoutButton } from "@inrupt/solid-ui-react";
-import { Button, TextField, FormGroup, Container } from "@material-ui/core";
+import { LoginButton, LogoutButton, CombinedDataProvider, Image, useSession} from "@inrupt/solid-ui-react";
+import { Button, TextField, FormGroup, CardActionArea, IconButton, MoreVertIcon, Avatar } from "@material-ui/core";
 import App from "../App"
 import logo from '../images/lomapLogo.png';
+import { VCARD } from "@inrupt/lit-generated-vocab-common";
 
 
 export default function Navbar({logggin}){
   const [idp, setIdp] = useState("https://inrupt.net");
   const [currentUrl, setCurrentUrl] = useState("https://localhost:3000");
+  const { session } = useSession();
+  const { webId } = session.info;
 
   useEffect(() => {
     setCurrentUrl(window.location.href);
@@ -15,26 +18,34 @@ export default function Navbar({logggin}){
   
     return (
         <nav className="nav">
+
           <a href="/home">
             <img src={logo} alt="Logo de Lomap"/>
           </a>
-          <FormGroup>
 
-          {({logggin}) ? 
-              <LoginButton oidcIssuer={idp} redirectUrl={currentUrl}>
+          
+          {logggin ? (
+            <>
+              <CombinedDataProvider datasetUrl={webId} thingUrl={webId} className="image-button">  
+                  <Image property={VCARD.hasPhoto.iri.value} alt="Foto de perfil del usuario" style={{width:80, height:80, borderRadius:40}}/>     
+              </CombinedDataProvider>
+              <LogoutButton >
+                <Button style={{ marginTop: 20 }} variant="contained" color="primary">
+                  Logout
+                </Button>
+              </LogoutButton>
+            </>
+          ) : (
+            <>
+              <LoginButton oidcIssuer={idp} redirectUrl={currentUrl} className="login_button">
                 <Button variant="contained" color="primary">
                   Login
                   </Button>
               </LoginButton>
-          : 
-          <LogoutButton >
-            <Button style={{ marginTop: 20 }} variant="contained" color="primary">
-              Logout
-            </Button>
-          </LogoutButton>
-          }
+            </>
+          )}
 
-          </FormGroup>
+          
         </nav>
         
       )
