6025914e86e1646dd5430cd6cd15b6e12ab70219
juani <juaniteje@gmail.com>
2023-03-26 17:54:46 +0200

    Se han añadido los tests de la parte de comentarios

---
+++ webapp/src/components/Comments/CommentsTests.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from "@testing-library/react";
+import UserList from "./UserList";
+import {User} from "../shared/shareddtypes";
+
+test('Check that it has name and text', async () => {
+    const {getByText} = render(<Comments username={"a"}/>);
+    expect(getByText()).toBeInTheDocument();
+});
