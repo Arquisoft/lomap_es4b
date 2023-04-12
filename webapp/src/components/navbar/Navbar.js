15f4e9d57d7d7877763b22b4f357b200068a2a19
juani <juaniteje@gmail.com>
2023-04-12 17:57:34 +0200

    Se ha añadido la posibilidad de cambiar de proveedor

--- webapp/src/components/navbar/Navbar.js
+++ webapp/src/components/navbar/Navbar.js
@@ -6,50 +6,66 @@ import { VCARD, FOAF } from "@inrupt/lit-generated-vocab-common";
 import "./NavBar.css"
 
 export default function Navbar(props){
-  const [idp, setIdp] = useState("https://inrupt.net");
-  const [currentUrl, setCurrentUrl] = useState("https://localhost:3000");
-  const { session } = useSession();
-  const { webId } = session.info;
-  const {logggin} = props;
+    const [idp, setIdp] = useState("https://inrupt.net/");
+    const [currentUrl, setCurrentUrl] = useState("https://localhost:3000/");
+    const { session } = useSession();
+    const { webId } = session.info;
+    const {logggin} = props;
+    const listaProveedores = [{url:"https://inrupt.net/"}, {url:"https://solidcommunity.net/"}];
 
-  useEffect(() => {
-    setCurrentUrl(window.location.href);
-  }, [setCurrentUrl]);
-  
+    useEffect(() => {
+        setCurrentUrl(window.location.href);
+    }, [setCurrentUrl]);
+
+    const handleChange = (event)=>{
+        const value = event.target.value;
+        setIdp(value);
+    }
     return (
         <nav className="navBar">
 
-          <a href="/home">
-            <img src={logo} alt="Logo de Lomap"/>
-          </a>
+            <a href="/home">
+                <img src={logo} alt="Logo de Lomap"/>
+            </a>
 
-          
-          {logggin ? (
-            <>
-              <div className="navProfile">
-              <CombinedDataProvider datasetUrl={webId} thingUrl={webId} className="image-button" style={{flexDirection: 'row'}}>
-                  <Image property={VCARD.hasPhoto.iri.value} alt="Foto de perfil del usuario" style={{width:70, height:70, borderRadius:40}}/>
-                  <Text property={FOAF.name.iri.value} style={{ fontSize: 18 }}/>
-                </CombinedDataProvider> 
-                <LogoutButton >
-                  <Button variant="contained" color="primary">
-                    Logout
-                  </Button>
-                </LogoutButton>
-              </div>
-            </>
-          ) : (
-            <>
-              <LoginButton oidcIssuer={idp} redirectUrl={currentUrl} className="login_button">
-                <Button variant="contained" color="primary" /* onClick={savePoints(session,webId)} */>
-                  Login
-                  </Button>
-              </LoginButton> 
-            </>
-          )}
 
-          
+            {logggin ? (
+                <>
+                    <div className="navProfile">
+                        <CombinedDataProvider datasetUrl={webId} thingUrl={webId} className="image-button" style={{flexDirection: 'row'}}>
+                            <Image property={VCARD.hasPhoto.iri.value} alt="Foto de perfil del usuario" style={{width:70, height:70, borderRadius:40}}/>
+                            <Text property={FOAF.name.iri.value} style={{ fontSize: 18 }}/>
+                        </CombinedDataProvider>
+                        <LogoutButton >
+                            <Button variant="contained" color="primary">
+                                Logout
+                            </Button>
+                        </LogoutButton>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <select value={idp} onChange={handleChange}>
+                        <option value="a">Selecciona un Proveedor</option>
+                        {listaProveedores.map((proveedor) => (
+                            <option key={proveedor.url} value={proveedor.url}>
+                                {proveedor.url}
+                            </option>
+                        ))}
+                    </select>
+                    <LoginButton oidcIssuer={idp} redirectUrl={currentUrl} className="login_button">
+                        <Button type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}>
+                            Login
+                        </Button>
+                    </LoginButton>
+                </>
+            )}
+
+
         </nav>
-        
-      )
+
+    )
 }
