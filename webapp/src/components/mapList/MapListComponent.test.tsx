20d3555b717f5a6038d343cebcf77ff20d29d38b
Daniel Suárez <uo276467@uniovi.es>
2023-04-27 01:43:33 +0200

    añadido await en render

--- webapp/src/components/mapList/MapListComponent.test.tsx
+++ webapp/src/components/mapList/MapListComponent.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import MapListComponent from "./MapListComponent.js";
 
 test('check that the login form renders propertly', async () => {
-    const {getByTestId} = render(<MapListComponent mapId={'1'} setCurrentMapId={() => {}} session={[]} webId={"https://uo281835.inrupt.net/profile/card#me"}
+    const {getByTestId} = await render(<MapListComponent mapId={'1'} setCurrentMapId={() => {}} session={[]} webId={"https://uo281835.inrupt.net/profile/card#me"}
                         showMapPoints={() => {}}
                         />);
     getByTestId("mapComponentSubmit").click(); 
