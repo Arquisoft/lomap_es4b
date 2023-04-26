d71291a32acb18de4c2d268aa69c73d86e446576
juani <juaniteje@gmail.com>
2023-04-26 14:07:35 +0200

    Se han arreglado algunos tests y se ha añadido la funcionalidad del filtro por todas las categorias

--- webapp/src/components/friendsList/FriendComponent.test.tsx
+++ webapp/src/components/friendsList/FriendComponent.test.tsx
@@ -1,13 +1,22 @@
 import { render } from "@testing-library/react";
+import { useState, useEffect} from "react";
 import FriendComponent from "./FriendComponent";
 import React from "react";
+import {act} from "react-dom/test-utils";
+import ProfileViewer from "../profileviewer/ProfileViewer";
+import { ProSidebarProvider } from "react-pro-sidebar";
+import {useSession} from "@inrupt/solid-ui-react";
+
 
 test('check that the FriendComponent renders properly', async () => {
-    const {getByTestId} = await render(<FriendComponent  friendURL={"https://uo281835.inrupt.net/profile/card#me"} friendName={"item.friendName"} setCurrentMapId={()=>{}} session={[]} key={"https://uo281835.inrupt.net/profile/card#me"}/>);
+
+    //const {session}= useSession()
+    const {getByTestId} = await render(<FriendComponent  friendURL={"https://uo281835.inrupt.net/profile/card#me"}
+                                                         friendName={"item.friendName"}
+                                                         setCurrentMapId={()=>{}} session={[]} key={"https://uo281835.inrupt.net/profile/card#me"}/>);
 
     expect(getByTestId("nombreAmigoParrafo")).toBeInTheDocument();
     expect(getByTestId("nombreAmigoParrafo")).toBeVisible();
     expect(getByTestId("botonFriendComponent")).toBeInTheDocument();
     expect(getByTestId("botonFriendComponent")).toBeVisible();
-    getByTestId("botonFriendComponent").click();
 });
--- webapp/src/components/login/LoginForm.test.tsx
+++ webapp/src/components/login/LoginForm.test.tsx
@@ -1,10 +1,16 @@
 import { render } from "@testing-library/react";
 import LoginForm from "./LoginForm";
+import {act} from "react-dom/test-utils";
 
 test('check that the login form renders propertly', async () => {
     const {getByTestId} = await render(<LoginForm/>);
-    expect(getByTestId("containerLoginForm")).toBeInTheDocument();
-    expect(getByTestId("containerLoginForm")).toBeVisible();
-    expect(getByTestId("homeLoginForm")).toBeInTheDocument();
-    expect(getByTestId("homeLoginForm")).toBeVisible();
+    act(()=>{
+
+       expect(getByTestId("containerLoginForm")).toBeInTheDocument();
+       expect(getByTestId("containerLoginForm")).toBeVisible();
+       expect(getByTestId("homeLoginForm")).toBeInTheDocument();
+       expect(getByTestId("homeLoginForm")).toBeVisible();
+
+   });
+
   });
