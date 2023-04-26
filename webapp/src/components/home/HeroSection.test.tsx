f5170cf8f4335fef34697e9cdca984d06cff21d8
Daniel Suárez <uo276467@uniovi.es>
2023-04-27 01:50:08 +0200

    comentado addFriendForm

--- webapp/src/components/home/HeroSection.test.tsx
+++ webapp/src/components/home/HeroSection.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import HeroSection from "./HeroSection";
 
 test('check that the login form renders propertly', async () => {
-    const {getByText} = render(<HeroSection />);
+    const {getByText} = await render(<HeroSection />);
     expect(getByText("LOMAP")).toBeInTheDocument();
     expect(getByText("LOMAP")).toBeVisible();
     expect(getByText("Crea tus propios mapas")).toBeInTheDocument();
