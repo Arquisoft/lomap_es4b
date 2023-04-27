436d5b222058109766b7e59401c9a30f7d749051
Aaron <Aaron@DESKTOP-8HG00E2>
2023-04-27 19:55:17 +0200

    descomentado yml y test de markerList

--- webapp/src/components/markersList/Markerslist.test.tsx
+++ webapp/src/components/markersList/Markerslist.test.tsx
@@ -1,17 +1,7 @@
 import { render } from "@testing-library/react";
 import Markerslist from "./Markerslist.js";
 
-// jest.mock("@inrupt/solid-ui-react", () => ({
-//     useSession: () => ({
-//       session: {
-//         info: {
-//           webId: "https://uo276467.inrupt.net/profile/card#me",
-//         },
-//       },
-//     }),
-//   }));
-
 test('check that the login form renders propertly', async () => {
-    // const {getByTestId} = await render(<Markerslist setPointsLoading={() => {}} centerMap={() => {}} mapId={1} webId={"https://uo276467.inrupt.net/profile/card#me"}/>);
-    // expect(getByTestId("sideListLocations")).toBeInTheDocument();
+    const {getByTestId} = await render(<Markerslist setPointsLoading={() => {}} centerMap={() => {}} mapId={1} webId={"https://uo276467.inrupt.net/profile/card#me"}/>);
+    expect(getByTestId("sideListLocations")).toBeInTheDocument();
 });
