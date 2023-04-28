f57c09ca2fab146cd1fc4fc67961247839ad2489
Daniel Suárez <uo276467@uniovi.es>
2023-04-28 10:35:44 +0200

    modificado jest.config

--- webapp/e2e/jest.config.ts
+++ webapp/e2e/jest.config.ts
@@ -6,7 +6,7 @@ export default {
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     moduleNameMapper:{"^uuid$": "uuid"},
     preset: "jest-puppeteer",
-    // collectCoverage: true,
+    collectCoverage: true,
     collectCoverageFrom: ["../src/components/**"],
     testTimeout: 200000
 }
