638a5aa77705686495786239d05da67ec65e6461
Daniel Suárez <uo276467@uniovi.es>
2023-04-28 11:12:49 +0200

    mejorada duplicidad de código y exlcuidos algunos tests

--- webapp/src/components/sidebar/SideBar.test.tsx
+++ webapp/src/components/sidebar/SideBar.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import SideBarConProviderSOLOPARATESTS from "./SideBarConProviderSOLOPARATESTS";
 
-test('check that the sidebar renders propertly', async () => {
+test('check that the sidebar renders and functions propertly', async () => {
 
     const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
     expect(getByTestId("sidebar")).toBeInTheDocument();
@@ -13,63 +13,76 @@ test('check that the sidebar renders propertly', async () => {
     expect(getByTestId("sidebarAddFriend")).toBeInTheDocument();
     expect(getByTestId("sidebarAbout")).toBeInTheDocument();
 
+    //Botón superior
+    getByTestId("sidebarFirst").click();
+    getByTestId("sidebarFirst").click();
+
+    //Puntos
+    getByTestId("sidebarFirst").click();
+    getByTestId("sidebarPoints").click();
+    getByTestId("sidebarViewPoints").click();  
+    getByTestId("sidebarViewPoints").click();  
+    getByTestId("sidebarFilterPoints").click();     
+    getByTestId("sidebarFilterPoints").click();     
+    getByTestId("sidebarPoints").click();
+    getByTestId("sidebarPoints").click();
+
+    //Mapas
+    getByTestId("sidebarFirst").click();
+    getByTestId("sidebarMapas").click();
+    getByTestId("sidebarViewMaps").click();
+    getByTestId("sidebarViewMaps").click();
+    getByTestId("sidebarAddMap").click();
+    getByTestId("sidebarAddMap").click();
+    getByTestId("sidebarMapas").click();
+
+    //Amigos
+    getByTestId("sidebarFirst").click();
+    getByTestId("sidebarFriends").click();
+    getByTestId("sidebarFriends").click();
+    getByTestId("sidebarAddFriend").click();
+    getByTestId("sidebarAddFriend").click();
+
+    //About
+    getByTestId("sidebarFirst").click();
+    getByTestId("sidebarAbout").click();
+    getByTestId("sidebarAbout").click();
+
 });
 
-test('check that the sidebar point section can be clicked propertly', async () => {
+// test('check that the sidebar point section can be clicked propertly', async () => {
     
-    const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
-    expect(getByTestId("sidebarPoints")).toBeInTheDocument();
-    getByTestId("sidebarFirst").click();
-    getByTestId("sidebarPoints").click();
-    getByTestId("sidebarViewPoints").click();  
-    getByTestId("sidebarViewPoints").click();  
-    getByTestId("sidebarFilterPoints").click();     
-    getByTestId("sidebarFilterPoints").click();     
-    getByTestId("sidebarPoints").click();
-    getByTestId("sidebarPoints").click();
+//     const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
+//     expect(getByTestId("sidebarPoints")).toBeInTheDocument();
+    
 
-});
+// });
 
-test('check that the sidebar first section can be clicked propertly', async () => {
+// test('check that the sidebar first section can be clicked propertly', async () => {
 
-    const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
-    expect(getByTestId("sidebarFirst")).toBeInTheDocument();
-    getByTestId("sidebarFirst").click();
-    getByTestId("sidebarFirst").click();
-});
+//     const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
+//     expect(getByTestId("sidebarFirst")).toBeInTheDocument();
+    
+// });
 
-test('check that the sidebar map section can be clicked propertly', async () => {
+// test('check that the sidebar map section can be clicked propertly', async () => {
 
-    const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
-    expect(getByTestId("sidebarMapas")).toBeInTheDocument();
-    getByTestId("sidebarFirst").click();
-    getByTestId("sidebarMapas").click();
-    getByTestId("sidebarViewMaps").click();
-    getByTestId("sidebarViewMaps").click();
-    getByTestId("sidebarAddMap").click();
-    getByTestId("sidebarAddMap").click();
-    getByTestId("sidebarMapas").click();
+//     const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
+//     expect(getByTestId("sidebarMapas")).toBeInTheDocument();
+    
 
-});
+// });
 
-test('check that the sidebar friends section can be clicked propertly', async () => {
+// test('check that the sidebar friends section can be clicked propertly', async () => {
 
-    const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
-    expect(getByTestId("sidebarFriends")).toBeInTheDocument();
-    getByTestId("sidebarFirst").click();
-    getByTestId("sidebarFriends").click();
-    getByTestId("sidebarFriends").click();
-    getByTestId("sidebarAddFriend").click();
-    getByTestId("sidebarAddFriend").click();
+//     const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
+//     expect(getByTestId("sidebarFriends")).toBeInTheDocument();
 
-});
+// });
 
-test('check that the about section can be clicked propertly', async () => {
+// test('check that the about section can be clicked propertly', async () => {
 
-    const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
-    expect(getByTestId("sidebarAbout")).toBeInTheDocument();
-    getByTestId("sidebarFirst").click();
-    getByTestId("sidebarAbout").click();
-    getByTestId("sidebarAbout").click();
+//     const {getByTestId} = await render(<SideBarConProviderSOLOPARATESTS/>);
+//     expect(getByTestId("sidebarAbout")).toBeInTheDocument();
 
-});
+// });
