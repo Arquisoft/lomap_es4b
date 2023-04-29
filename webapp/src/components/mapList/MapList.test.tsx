803b2ea81eba32cece81796814e80a3d2c835898
Daniel Suárez <uo276467@uniovi.es>
2023-04-29 19:52:39 +0200

    corregido testid de map list

--- webapp/src/components/mapList/MapList.test.tsx
+++ webapp/src/components/mapList/MapList.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import MapList from "./MapList.js";
 
-test('check that the login form renders propertly', async () => {
+test('check that the map list renders propertly', async () => {
     const {getByTestId} = await render(<MapList setMapsLoading={() => {}} session={[]} webId={"https://uo281835.inrupt.net/profile/card#me"}/>);
     expect(getByTestId("mapListComponent")).toBeInTheDocument();
 
