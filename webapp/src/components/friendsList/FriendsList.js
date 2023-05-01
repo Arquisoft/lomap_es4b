e2d58952cc8d23b07cf7f36596bb4e54f0cf99c8
Daniel Suárez <uo276467@uniovi.es>
2023-05-01 16:45:15 +0200

    corregir carga de eliminar amigo

--- webapp/src/components/friendsList/FriendsList.js
+++ webapp/src/components/friendsList/FriendsList.js
@@ -12,11 +12,11 @@ export function FriendsList(props) {
       const fetchFriends = async() => {
           setFriendsLoading(true);
           const result = await getAllFriendsFromPod(webId);
-          setFriendsLoading(false);
           setFriends(result);
+          setFriendsLoading(false);
       }
       fetchFriends();
-    }, []);
+    }, [friends]);
 
     return (
       <div className='friendListComponent'>
